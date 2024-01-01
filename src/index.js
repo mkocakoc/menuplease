546b6211e18d1cd01d8b45102bfe3d9549e9cb49
mkocakoc <mkocakocfastking@gmail.com>
2024-01-01 23:38:54 +0300

    menu display changed and some css added

--- src/index.js
+++ src/index.js
@@ -50,22 +50,82 @@ const pizzaData = [
 
 
 function App() {
-  
+
   return (
     <div className="App">
-     <h1>Menu</h1>
-      <Pizza />
+      <div className='container'>
+        <Header />
+        <Menu />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+function Header() {
+  return (
+    <header>
+      <h1>QR Menu Company Name </h1>
+    </header>
+  );
+}
+function Menu() {
+  return (
+    <div className='menu'>
+      <h2>OUR MENU</h2>
+   
+    
+      <div className='menuFlex'>
+        {pizzaData.map((pizza, index) => (
+          <div className={pizza.soldOut ? 'menuFlexItem sold-out' : 'menuFlexItem'} key={index}>
+            <img src={pizza.photoName} alt={pizza.name} />
+            <h3>{pizza.name}</h3>
+            <p>{pizza.ingredients}</p>
+            <div className='line'>
+              <p>{pizza.price} €</p>
+              {pizza.soldOut && <p className='soldout'>Sold Out</p>}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-function Pizza(){
+function Footer() {
   return (
-  <div>
-    <img src='pizzas/focaccia.jpg' alt='Focaccia' />
-    <h2>Focaccia</h2>
-    <p>Bread with italian olive oil and rosemary</p>
-  </div>  );
+    <footer>
+
+      <h1>Time: {new Date().toLocaleTimeString()}</h1>
+      <h2>All rights reserved</h2>
+    </footer>
+  );
+  //return React.createElement("footer", null, "All rights reserved");
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src='pizzas/focaccia.jpg' alt='Focaccia' />
+      <h2>Focaccia</h2>
+      <p>Bread with italian olive oil and rosemary</p>
+    </div>);
+}
+
+function Pizza2() {
+  return (
+    <div>
+      <img src='pizzas/margherita.jpg' alt='Margherita' />
+      <h2>Margherita</h2>
+      <p>Tomato and mozarella</p>
+    </div>);
+}
+function Pizza3() {
+  return (
+    <div>
+      <img src='pizzas/spinaci.jpg' alt='Spinaci' />
+      <h2>Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>);
 }
 
 
@@ -83,3 +143,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
